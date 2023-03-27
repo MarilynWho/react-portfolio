6696e50b5bc2663576802eb81607b44f7e675cc0
MarilynWho <viktoriia.cheche@gmail.com>
2023-03-27 01:21:39 +0100

    Create Footer and projects

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const footer = () => {
+  return (
+    <div>
+      <footer>
+        <p>
+          © 2023 Victoria Chechelnytska
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default footer;
